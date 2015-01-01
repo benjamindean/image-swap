aa5fec7136fba7bbd48c504f9b6509bdc231ed02
Benjamin Dean <berman.reshetnikov@gmail.com>
2015-01-01 22:24:38 +0200

    Resolutions count are not limited anymore

--- image-swap.js
+++ image-swap.js
@@ -1,25 +1,4 @@
-// Copyright (c) 2014 Benjamin Dean
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
-
-(function (window, document, undefined) {
+(function(window, document, undefined) {
 
     'use strict';
 
@@ -29,11 +8,11 @@
 
     ImageSwap.prototype = {
 
-        init: function (options) {
+        init: function(options) {
             this.settings(options);
             this.run();
         },
-        settings: function (options) {
+        settings: function(options) {
             this.config = {
                 picture: options.picture || this.config.picture,
                 path: options.path || this.config.path,
@@ -41,29 +20,27 @@
                 resolutions: options.resolutions || this.config.resolutions
             };
         },
-        getWidth: function () {
+        getWidth: function() {
             return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
         },
-        run: function () {
+        _setSrc: function(size) {
+            return this.config.picture.setAttribute("src", this.config.path + size + "/" + this.config.name);
+        },
+        run: function() {
 
-            var width = this.getWidth();
-            var config = this.config;
+            var width = this.getWidth(),
+                config = this.config,
+                index;
 
-            switch (true) {
-                case width <= config.resolutions.small:
-                    config.picture.setAttribute("src", config.path + config.resolutions.small + "/" + config.name);
-                    break;
-                case width <= config.resolutions.medium:
-                    config.picture.setAttribute("src", config.path + config.resolutions.medium + "/" + config.name);
-                    break;
-                case width <= config.resolutions.large:
-                    config.picture.setAttribute("src", config.path + config.resolutions.large + "/" + config.name);
-                    break;
-                default:
+            for (index in config.resolutions) {
+                if (width <= config.resolutions[index]) {
+                    this._setSrc(config.resolutions[index]);
+                } else {
                     config.picture.setAttribute("src", config.path + config.name);
+                }
             }
         },
-        update: function () {
+        update: function() {
             this.run();
         }
     };
